031e73729c02c2e7523f81e6b0e538a0276e67fc
Arjan <arjan@arjancodes.com>
2022-08-17 13:34:40 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+# write the answer to a source file
+echo "#!/usr/bin/env Rscript\n\n $answer" > ./src/script.r
+
+# turn the script into an executable
+chmod +x script.r
+
+# run the script
+./script.r
